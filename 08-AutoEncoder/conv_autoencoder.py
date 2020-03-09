50d3df7da57dd69df4d357128e501ecb6dc7ab61
cclamb <cclamb@sandia.gov>
2020-03-09 10:38:01 -0600

    refactoring

--- 08-AutoEncoder/conv_autoencoder.py
+++ 08-AutoEncoder/conv_autoencoder.py
@@ -10,28 +10,9 @@ from torchvision.utils import save_image
 from torchvision.datasets import MNIST
 import os
 
-if not os.path.exists('./dc_img'):
-    os.mkdir('./dc_img')
-
-
-def to_img(x):
-    x = 0.5 * (x + 1)
-    x = x.clamp(0, 1)
-    x = x.view(x.size(0), 1, 28, 28)
-    return x
-
-
 num_epochs = 100
 batch_size = 128
 learning_rate = 1e-3
-
-img_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.5], [0.5])  # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-
-dataset = MNIST('./data', transform=img_transform, download=True)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
 class AutoEncoder(nn.Module):
@@ -60,29 +41,52 @@ class AutoEncoder(nn.Module):
         return x
 
 
-device = torch.device(("cuda" if torch.cuda.is_available() else "cpu"))
-model = AutoEncoder()
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
-                             weight_decay=1e-5)
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
 
-for epoch in range(num_epochs):
-    for data in dataloader:
-        img, _ = data
-        img = Variable(img).cuda()
-        # ===================forward=====================
-        output = model(img)
-        loss = criterion(output, img)
-        # ===================backward====================
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    # ===================log========================
-    print('epoch [{}/{}], loss:{:.4f}'
-          .format(epoch+1, num_epochs, loss.item()))
 
-    if epoch % 10 == 0:
-        pic = to_img(output.cpu().data)
-        save_image(pic, './dc_img/image_{}.png'.format(epoch))
+def main():
+    if not os.path.exists('./dc_img'):
+        os.mkdir('./dc_img')
 
-torch.save(model.state_dict(), './conv_autoencoder.pth')
+    img_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])  # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    dataset = MNIST('./data', transform=img_transform, download=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    torch.device(("cuda" if torch.cuda.is_available() else "cpu"))
+    model = AutoEncoder()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
+                                 weight_decay=1e-5)
+
+    for epoch in range(num_epochs):
+        for data in dataloader:
+            img, _ = data
+            img = Variable(img).cuda()
+            # ===================forward=====================
+            output = model(img)
+            loss = criterion(output, img)
+            # ===================backward====================
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # ===================log========================
+        print('epoch [{}/{}], loss:{:.4f}'
+              .format(epoch+1, num_epochs, loss.item()))
+
+        if epoch % 10 == 0:
+            pic = to_img(output.cpu().data)
+            save_image(pic, './dc_img/image_{}.png'.format(epoch))
+
+    torch.save(model.state_dict(), './conv_autoencoder.pth')
+
+
+if __name__ == "__main__":
+    main()
--- 08-AutoEncoder/simple_autoencoder.py
+++ 08-AutoEncoder/simple_autoencoder.py
@@ -11,28 +11,9 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 
-if not os.path.exists('./mlp_img'):
-    os.mkdir('./mlp_img')
-
-
-def to_img(x):
-    x = 0.5 * (x + 1)
-    x = x.clamp(0, 1)
-    x = x.view(x.size(0), 1, 28, 28)
-    return x
-
-
 num_epochs = 100
 batch_size = 128
 learning_rate = 1e-3
-
-img_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.5], [0.5])  # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-
-dataset = MNIST('./data', transform=img_transform, download=True)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
 class AutoEncoder(nn.Module):
@@ -57,28 +38,51 @@ class AutoEncoder(nn.Module):
         return x
 
 
-device = torch.device(("cuda" if torch.cuda.is_available() else "cpu"))
-model = AutoEncoder()
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
 
-for epoch in range(num_epochs):
-    for data in dataloader:
-        img, _ = data
-        img = img.view(img.size(0), -1)
-        img = Variable(img)
-        # ===================forward=====================
-        output = model(img)
-        loss = criterion(output, img)
-        # ===================backward====================
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    # ===================log========================
-    print('epoch [{}/{}], loss:{:.4f}'
-          .format(epoch + 1, num_epochs, loss.item()))
-    if epoch % 10 == 0:
-        pic = to_img(output.cpu().data)
-        save_image(pic, './mlp_img/image_{}.png'.format(epoch))
 
-torch.save(model.state_dict(), './sim_autoencoder.pth')
+def main():
+    if not os.path.exists('./mlp_img'):
+        os.mkdir('./mlp_img')
+
+    torch.device(("cuda" if torch.cuda.is_available() else "cpu"))
+    model = AutoEncoder()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+    img_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])  # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    dataset = MNIST('./data', transform=img_transform, download=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    for epoch in range(num_epochs):
+        for data in dataloader:
+            img, _ = data
+            img = img.view(img.size(0), -1)
+            img = Variable(img)
+            # ===================forward=====================
+            output = model(img)
+            loss = criterion(output, img)
+            # ===================backward====================
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # ===================log========================
+        print('epoch [{}/{}], loss:{:.4f}'
+              .format(epoch + 1, num_epochs, loss.item()))
+        if epoch % 10 == 0:
+            pic = to_img(output.cpu().data)
+            save_image(pic, './mlp_img/image_{}.png'.format(epoch))
+
+    torch.save(model.state_dict(), './sim_autoencoder.pth')
+
+
+if __name__ == "__main__":
+    main()
