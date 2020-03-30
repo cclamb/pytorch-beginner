59ca50b61f27d20bedf7c611df8a11e692a74afc
Chris Lamb <cclamb@unm.edu>
2020-03-30 14:31:57 -0600

    support for data parallelism works with no parallelism :-)

--- 08-AutoEncoder/conv_autoencoder.py
+++ 08-AutoEncoder/conv_autoencoder.py
@@ -16,19 +16,20 @@ import ipdb
 num_epochs = 100
 batch_size = 200
 learning_rate = 1e-3
-number_of_devices = 1
 
 clamp = False
 
 class AutoEncoder(nn.Module):
-    def __init__(self, is_clamping=True):
+    def __init__(self, is_clamping=True, number_processors=1, batch_size=100):
         super(AutoEncoder, self).__init__()
 
         self.is_clamping = is_clamping
 
         self.latent_dim = 3
+        self.number_processors = number_processors
         DCIGNClamping.latent_dim = self.latent_dim
         self.dcign = DCIGNClamping.apply
+        self.batch_size = batch_size
 
         self.conv2d_1 = nn.Conv2d(1, 16, 3, stride=3, padding=1)  # b, 16, 10, 10
         self.relu_1 = nn.ReLU(True)
@@ -36,9 +37,9 @@ class AutoEncoder(nn.Module):
         self.conv2d_2 = nn.Conv2d(16, 8, 3, stride=2, padding=1)  # b, 8, 3, 3
         self.relu_2 = nn.ReLU(True)
         self.max_pool_2d_2 = nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
-        self.fc = nn.Linear(int(batch_size * 32 / number_of_devices), self.latent_dim)
+        self.fc = nn.Linear(int(self.batch_size * 32 / self.number_processors), self.latent_dim)
 
-        self.d_fc = nn.Linear(self.latent_dim, int(batch_size * 32 / number_of_devices))
+        self.d_fc = nn.Linear(self.latent_dim, int(self.batch_size * 32 / self.number_processors))
         self.d_conv_trans_2d_1 = nn.ConvTranspose2d(8, 16, 3, stride=2)  # b, 16, 5, 5
         self.d_relu_1 = nn.ReLU(True)
         self.d_conv_trans_2d_2 = nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1)  # b, 8, 15, 15
@@ -59,12 +60,12 @@ class AutoEncoder(nn.Module):
         h4 = self.conv2d_2(h3)
         h5 = self.relu_2(h4)
         h6 = self.max_pool_2d_2(h5)
-        h6 = h6.view(-1, int(batch_size * 32 / number_of_devices))
+        h6 = h6.view(-1, int(self.batch_size * 32 / self.number_processors))
         return self.fc(h6)
 
     def decode(self, x):
         h0 = self.d_fc(x)
-        h0 = h0.view([int(batch_size / number_of_devices), 8, 2, 2])
+        h0 = h0.view([int(self.batch_size / self.number_processors), 8, 2, 2])
         h1 = self.d_conv_trans_2d_1(h0)
         h2 = self.d_relu_1(h1)
         h3 = self.d_conv_trans_2d_2(h2)
@@ -97,7 +98,11 @@ def main():
 
     number_of_devices = torch.cuda.device_count() if torch.cuda.is_available() else 1
     device = torch.device(("cuda" if torch.cuda.is_available() else "cpu"))
-    model = AutoEncoder(is_clamping=clamp).to(device)
+    model = AutoEncoder(
+        is_clamping=clamp,
+        number_processors=number_of_devices,
+        batch_size=batch_size
+    ).to(device)
 
     if torch.cuda.is_available() and torch.cuda.device_count() > 1:
         print("Let's use", number_of_devices, "GPUs!")
