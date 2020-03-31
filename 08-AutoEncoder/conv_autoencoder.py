978eca9b167721e05d14a4594533256b86eb4de0
Chris Lamb <cclamb@unm.edu>
2020-03-30 20:01:12 -0600

    tuning convnet

--- 08-AutoEncoder/conv_autoencoder.py
+++ 08-AutoEncoder/conv_autoencoder.py
@@ -25,7 +25,7 @@ class AutoEncoder(nn.Module):
 
         self.is_clamping = is_clamping
 
-        self.latent_dim = 10
+        self.latent_dim = 64
         self.number_processors = number_processors
         DCIGNClamping.latent_dim = self.latent_dim
         self.dcign = DCIGNClamping.apply
